d9d81d3c8102bc3fa6c9105aaeff33120f77a4c6
CommunityCoinDev <dev@communitycoin.world>
2019-07-05 09:17:08 +0200

    upgrading pow startup phase with 1000 blocks

--- src/main.h
+++ src/main.h
@@ -25,7 +25,7 @@ class CInv;
 class CRequestTracker;
 class CNode;
 
-static const int LAST_POW_BLOCK = 100; // disable POW after 100 initial blocks
+static const int LAST_POW_BLOCK = 1000; // disable POW after 1000 initial blocks
 
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
